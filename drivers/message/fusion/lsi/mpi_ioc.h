5369d9219e0dfa3d378377164974d6408926f258
Andrea Gelmini <andrea.gelmini@gelma.net>
2015-12-22 18:42:15 +0100

    Fix typos

--- drivers/message/fusion/lsi/mpi_ioc.h
+++ drivers/message/fusion/lsi/mpi_ioc.h
@@ -41,7 +41,7 @@
  *  08-08-01  01.02.01  Original release for v1.2 work.
  *                      New format for FWVersion and ProductId in
  *                      MSG_IOC_FACTS_REPLY and MPI_FW_HEADER.
- *  08-31-01  01.02.02  Addded event MPI_EVENT_SCSI_DEVICE_STATUS_CHANGE and
+ *  08-31-01  01.02.02  Added event MPI_EVENT_SCSI_DEVICE_STATUS_CHANGE and
  *                      related structure and defines.
  *                      Added event MPI_EVENT_ON_BUS_TIMER_EXPIRED.
  *                      Added MPI_IOCINIT_FLAGS_DISCARD_FW_IMAGE.
