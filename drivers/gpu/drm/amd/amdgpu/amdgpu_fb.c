3f8ac3322393944eb4e7a542d9d0c1ddc444c1f0
Andrea Gelmini <andrea.gelmini@gelma.net>
2015-12-22 18:39:53 +0100

    Fix typos

--- drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_fb.c
@@ -249,7 +249,7 @@ static int amdgpufb_create(struct drm_fb_helper *helper,
 	}
 
 	DRM_INFO("fb mappable at 0x%lX\n",  info->fix.smem_start);
-	DRM_INFO("vram apper at 0x%lX\n",  (unsigned long)adev->mc.aper_base);
+	DRM_INFO("vram aper at 0x%lX\n",  (unsigned long)adev->mc.aper_base);
 	DRM_INFO("size %lu\n", (unsigned long)amdgpu_bo_size(rbo));
 	DRM_INFO("fb depth is %d\n", fb->depth);
 	DRM_INFO("   pitch is %d\n", fb->pitches[0]);
