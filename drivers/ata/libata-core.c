e0df5d3afc0547f77c0ffb091d8a62a3cb18ffcb
Andrea Gelmini <andrea.gelmini@gelma.net>
2015-12-22 18:39:23 +0100

    Fix typos

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -6577,7 +6577,7 @@ static int __init ata_parse_force_one(char **cur,
 		return -EINVAL;
 	}
 	if (nr_matches > 1) {
-		*reason = "ambigious value";
+		*reason = "ambiguous value";
 		return -EINVAL;
 	}
 
