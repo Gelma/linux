a4ce33f3db0bf819b0835de42225a90acba10d60
Andrea Gelmini <andrea.gelmini@gelma.net>
2015-12-23 20:15:11 +0100

    Fix typo

--- drivers/staging/comedi/kcomedilib/kcomedilib_main.c
+++ drivers/staging/comedi/kcomedilib/kcomedilib_main.c
@@ -1,6 +1,6 @@
 /*
  * kcomedilib/kcomedilib.c
- * a comedlib interface for kernel modules
+ * a comedilib interface for kernel modules
  *
  * COMEDI - Linux Control and Measurement Device Interface
  * Copyright (C) 1997-2000 David A. Schleef <ds@schleef.org>
