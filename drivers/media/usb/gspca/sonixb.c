9530b503308e75df805b315d009be2345ae98f25
Andrea Gelmini <andrea.gelmini@gelma.net>
2015-12-26 12:47:06 +0100

    Fix typo

--- drivers/media/usb/gspca/sonixb.c
+++ drivers/media/usb/gspca/sonixb.c
@@ -220,7 +220,7 @@ static const __u8 initOv6650[] = {
 static const __u8 ov6650_sensor_init[][8] = {
 	/* Bright, contrast, etc are set through SCBB interface.
 	 * AVCAP on win2 do not send any data on this controls. */
-	/* Anyway, some registers appears to alter bright and constrat */
+	/* Anyway, some registers appears to alter bright and contrast */
 
 	/* Reset sensor */
 	{0xa0, 0x60, 0x12, 0x80, 0x00, 0x00, 0x00, 0x10},
